e0c268010d78fccf8cf56e1a53315632297f49a9
shamiulalamupom <1hourprograms@gmail.com>
2024-04-29 12:24:29 +0200

    (feat): CurrencyConverter data structure.

--- currency_converter.py
+++ currency_converter.py
@@ -1,3 +1,9 @@
+import requests
+import os
+from functools import lru_cache
+from dotenv import load_dotenv
+load_dotenv()
+
 currencies = {
     "USD": 1.0,
     "EUR": 0.83,
@@ -6,22 +12,29 @@ currencies = {
 }
 
 def convert_currency(amount, from_currency, to_currency):
-    rate = currencies[to_currency] / currencies[from_currency]
+    rate = to_currency / from_currency
     converted_amount = amount * rate
     return converted_amount
 
+@lru_cache(maxsize=None)
 def currency_converter():
     amount = float(input("Enter the amount: "))
-    from_currency = input("Enter the source currency (USD, EUR, GBP, JPY): ").upper()
-    to_currency = input("Enter the target currency (USD, EUR, GBP, JPY): ").upper()
+    from_currency_url = f"https://v6.exchangerate-api.com/v6/{os.getenv('Currency_ENV')}/latest/USD"
+    to_currency_url = f"https://v6.exchangerate-api.com/v6/{os.getenv('Currency_ENV')}/latest/USD"
+    inp_from_currency = input("Enter the source currency (USD, EUR, GBP, JPY): ").upper()
+    inp_to_currency = input("Enter the target currency (USD, EUR, GBP, JPY): ").upper()
 
-    if from_currency not in currencies or to_currency not in currencies:
-        print("Invalid currency")
-        return
+    while 1:
+        try:
+            from_currency = requests.get(from_currency_url).json()["conversion_rates"][inp_from_currency]
+            to_currency = requests.get(to_currency_url).json()["conversion_rates"][inp_to_currency]
+            break
+        except Exception:
+            print("There was some problem getting the data you are trying to get. Try again")
     
     converted_amount = convert_currency(amount, from_currency, to_currency)
 
-    print(f"{amount} {from_currency} = {converted_amount} {to_currency}")
+    print(f"{amount:.02f} {inp_from_currency} = {converted_amount:.02f} {inp_to_currency}")
 
 if __name__ == "__main__":
     currency_converter()
